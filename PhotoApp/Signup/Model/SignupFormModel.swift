7e4e9c8ea3dc1c34a1c2a21c1e3536584e2d0bf9
Mahshad <mahshad1368@gmail.com>
2024-08-27 15:44:02 +0200

    Create Signuo Form Model Struct

---
+++ PhotoApp/Signup/Model/SignupFormModel.swift
@@ -0,0 +1,18 @@
+//
+//  SignupFormModel.swift
+//  PhotoApp
+//
+//  Created by Mahshad Jafari on 27.08.24.
+//
+
+import Foundation
+
+
+struct SignupFormModel {
+    
+    let firstName: String
+    let lastName: String
+    let email: String
+    let password: String
+    let repeatPassword: String
+}
---
+++ PhotoAppTests/Signup/SignupPresenterTests.swift
@@ -0,0 +1,35 @@
+//
+//  SignupPresenterTests.swift
+//  PhotoAppTests
+//
+//  Created by Mahshad Jafari on 27.08.24.
+//
+
+import XCTest
+@testable import PhotoApp
+
+final class SignupPresenterTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testSignupPresenter_WhenInformationProvided_WillValidateEachProperty() {
+        //Arrange
+        let signupFormModel = SignupFormModel(firstName: "Mahshad", lastName: "Jafari", email: "test@test.com", password: "123456789", repeatPassword:  "123456789")
+        
+        //Act
+        
+        
+        //Assert
+    }
+}
